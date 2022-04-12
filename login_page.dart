543fd337eca91897ed713066aa69c7a8d6729937
duaaanis <103386530+duaaanis@users.noreply.github.com>
2022-04-12 13:30:16 +0500

    login

---
+++ login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPgae extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover,
+             ),
+             Text("Welcome",style: TextStyle(
+               fontSize: 22,
+               fontWeight: FontWeight.bold,
+             ),)
+        ],
+      )   
+    );
+  }
+}
